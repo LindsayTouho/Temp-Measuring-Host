b580b09c1c944fe91f265124c3be32f1ef08a88e
zhou_underlinux <1664214294@qq.com>
2017-09-18 21:39:18 +0800

    #

--- website/login.php
+++ website/login.php
@@ -1,6 +1,14 @@
 <?php
 	$userName = $_POST['userName'];
 	$passWd = $_POST['passWord'];
-	
-	echo 'user is '.$userName;
+
+	$db = new mysqli('47.93.191.3','public','Changli','temperature');
+	if(mysqli_connect_errno()){
+		echo 'ERROR : can not connect to database';
+		exit;
+	}
+	require('header.php');
+
+	require('footer.php');
+
 ?>
